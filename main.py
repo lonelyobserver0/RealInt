f58e85c1a567b23814ec8d214f563f279eb3c8fc
lonelyobserver <lcmulaz@gmail.com>
2025-05-21 23:12:48 +0200

    V3.1

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ from physics import setup_physics, step_physics
 from lab_scene import load_lab_scene
 from tracker import start_tracking, get_hand_positions
 
-start_tracking()
+app = Ursina()
+
+setup_physics()
+load_lab_scene()
 
 hand_spheres = [Entity(model='sphere', color=color.red, scale=0.1) for _ in range(21)]
 hand2_spheres = [Entity(model='sphere', color=color.green, scale=0.1) for _ in range(21)]
+
+camera.position = (0, 10, -20)
+camera.rotation_x = 20
 
 def update():
     step_physics(time.dt)
@@ -18,16 +24,5 @@ def update():
     if hands[1]:
         for i, pos in enumerate(hands[1]):
             hand2_spheres[i].position = pos
-
-app = Ursina()
-
-setup_physics()
-load_lab_scene()
-
-camera.position = (0, 10, -20)
-camera.rotation_x = 20
-
-def update():
-    step_physics(time.dt)
 
 app.run()
